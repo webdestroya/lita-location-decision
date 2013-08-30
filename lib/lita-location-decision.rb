4bea381dade0f53dfabb4331e9ac2a17f2c2be38
Mitch Dempsey <mrdempsey@gmail.com>
2013-08-30 14:24:13 -0700

    first

---
+++ lib/lita-location-decision.rb
@@ -0,0 +1,1 @@
+require "lita/handlers/location_decision"
---
+++ lib/lita/handlers/location_decision.rb
@@ -0,0 +1,10 @@
+require "lita"
+
+module Lita
+  module Handlers
+    class LocationDecision < Handler
+    end
+
+    Lita.register_handler(LocationDecision)
+  end
+end
